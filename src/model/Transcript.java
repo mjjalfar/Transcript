c13a8938fd0adadf87e65216ff722cde718aa31f
mjjalfar <40665689+mjjalfar@users.noreply.github.com>
2018-07-19 10:36:23 +0300

    Ammended the specification based on the lecture questions

--- src/model/Transcript.java
+++ src/model/Transcript.java
@@ -5,6 +5,7 @@ public class Transcript {
 
     //TODO: Design specification for this method
     //REQUIRES: The course is already part of the list of courses
+    //           the grade should be between 0.0 and 4.0, and/or the course should not be null
     //MODIFIES: this.
     //EFFECTS: Add grades to a course
     public void addGrade(String course, double grade){ }
@@ -13,13 +14,14 @@ public class Transcript {
     // This method should return course name and grade in some consistent String format
     //REQUIRES: Course exist, and has a grade already.
     //MODIFIES: nothing.
-    //EFFECTS: Return course name and grade in the form of Course, Grade
+    //EFFECTS: returns course name and grade in format CourseName: Grade
     public String getCourseAndGrade(String course){ return null; }
 
     //TODO: Design specification for this method
     //REQUIRES: nothing.
     //MODIFIES: nothing.
-    //EFFECTS: Print the transcript.
+    //EFFECTS:  Print the transcript.
+    //          prints course names with grades earned
     public void printTranscript(){ }
 
     //TODO: Design specification for this method
